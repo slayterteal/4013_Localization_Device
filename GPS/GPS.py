e7c58a9569a4bcf1f8cadd7117535d9559a6c3e3
Christopher Bird <77848443+crbird@users.noreply.github.com>
2021-11-05 02:09:11 -0500

    Add files via upload
    wanted to let you guys see where I was at. I'm most of the way there, fighting some bugs rn to get each part working together (also consistently working)

---
+++ GPS/GPS.py
@@ -0,0 +1,56 @@
+#will need to do serial magic that I dont understand...
+import serial #pip install pyserial
+import time
+
+mport = "/dev/ttyAMA0" #magic serial place gpio14 goes to
+
+def parseGPS(data):
+    if data[0:6] == "$GPGGA":
+        s = data.split(",")
+        if s[7] == '0' or s[7]=='00':
+            print ("no satellite data available")
+            return
+        lat = s[2]
+        lon = s[4]
+        #print("satellite(s): ", s[7])
+
+        time = s[1][0:2] + ":" + s[1][2:4] + ":" + s[1][4:6]
+        #print(time)
+
+        file = open("output.txt", "a")
+        file.write(s)
+        file.write("\n")
+        file.close()
+        print("data captured...")
+
+        return  lat,lon
+
+def decode(coord):
+    l = list(coord)
+    for i in range(0,len(l)-1):
+            if l[i] == "." :
+                    break
+    base = l[0:i-2]
+    degi = l[i-2:i]
+    degd = l[i+1:]
+    #print(base,"   ",degi,"   ",degd)
+    baseint = int("".join(base))
+    degiint = int("".join(degi))
+    degdint = float("".join(degd))
+    degdint = degdint / (10**len(degd))
+    degs = degiint + degdint
+    full = float(baseint) + (degs/60)
+    #print(full)
+    
+    return full
+
+ser = serial.Serial(mport,9600,timeout = 2)
+
+while True:
+    try:
+        dat = ser.readline().decode()
+        mylat,mylon = parseGPS(dat)
+        #print(mylat , "N " , mylon, "W")
+    except:
+        pass
+        #print("error")
