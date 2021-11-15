de9edfb4ca07d948f43e42f03fe4751819dc98a0
Christopher Bird <77848443+crbird@users.noreply.github.com>
2021-11-15 16:53:58 -0600

    Add files via upload

--- src/GPS.py
+++ src/GPS.py
@@ -1,56 +1,31 @@
-#will need to do serial magic that I dont understand...
 import serial #pip install pyserial
 import time
 
-mport = "/dev/ttyAMA0" #magic serial place gpio14 goes to
+mport = "/dev/serial0" #magic serial location of stuff
 
 def parseGPS(data):
-    if data[0:6] == "$GPGGA":
+    if data[0:6] == "$GPGGA": #other NMEA headers have info, but this header contains all the stuff we want
         s = data.split(",")
         if s[7] == '0' or s[7]=='00':
             print ("no satellite data available")
             return
-        lat = s[2]
-        lon = s[4]
-        print("satellite(s): ", s[7])
+        #print("satellite(s): ", s[7])
 
-        time = s[1][0:2] + ":" + s[1][2:4] + ":" + s[1][4:6]
-        print(time)
+        #uncomment if we dont want time in seconds only
+        #time = s[1][0:2] + ":" + s[1][2:4] + ":" + s[1][4:6]
+        #s[1] = time
+        
+        print(s)
 
-        return  str(s)
-
-def decode(coord):
-    l = list(coord)
-    for i in range(0,len(l)-1):
-            if l[i] == "." :
-                    break
-    base = l[0:i-2]
-    degi = l[i-2:i]
-    degd = l[i+1:]
-    #print(base,"   ",degi,"   ",degd)
-    baseint = int("".join(base))
-    degiint = int("".join(degi))
-    degdint = float("".join(degd))
-    degdint = degdint / (10**len(degd))
-    degs = degiint + degdint
-    full = float(baseint) + (degs/60)
-    #print(full)
-    
-    return full
+        return
 
 ser = serial.Serial(mport,9600,timeout = 2)
 
 while True:
-    # try:
-    dat = ser.readline().decode()
-    if len(dat) == 0:
-        data = "Nothing"
-    else:
-        data = "parseGPS()"
-        data = parseGPS(dat)
-        # print(mylat , "N " , mylon, "W")
-    print(data)
-    # except:
-        #pass
-        # print("error")
-        # exit()
+#for x in range(20):
+    try:
+        dat = ser.readline().decode()
+        parseGPS(dat)
+    except:
+        pass
+        #print("error")
